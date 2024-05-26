96b5ba36fa1da7db38a8f5c2ad6fc143c44d2d02
programCode-sudo <brandonjose2001@hotmail.com>
2024-05-25 20:00:51 -0600

    cambiando diseño de loggin

--- MarketingModule/views.py
+++ MarketingModule/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def hellowTest(request):
-    return render(request,'signup.html',{
-        'form': UserCreationForm
-    })
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Hacer algo después de que el formulario sea válido, como redirigir a otra página.
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
